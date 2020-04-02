1dca16c85c627becdfb1d3d4124e460cf167cf4d
vncz14 <57498723+vncz14@users.noreply.github.com>
2020-04-02 11:00:20 -0400

    init
    i really just copied code...

---
+++ index.js
@@ -0,0 +1,86 @@
+//Objects notes
+/* const Omor = { //curly brackets = it is an object
+    name: "Omor",
+    dob: 2003,
+    graduated: false,
+    age: function() {
+        console.log(this);
+        return 2020 - this.dob; // "this" refers to the current thing it is inside
+    }
+} //reference the whole object with Omor     reference one property with Omor.name */
+
+/* const Homer = {
+    name: "Homer",
+    age: 32,
+    family: {
+        wife: {
+            name: "Marge",
+            age: 33
+        },
+        son: {
+            name: "Bart",
+            age: 10
+        },
+    },
+    pets: ["Cat", "Dog"]
+} */
+
+
+
+
+// Destructuring notes
+
+/* const Sandra = {
+    name: "Sandra",
+    age: "17",
+    student: true,
+    year: "junior",
+    graduated: false
+}
+
+const {name, year} = Sandra; // you can only take things you need from something using this
+// const student = {}; //empty object
+// student.name = Sandra.name;
+const student = Sandra; // this is not a copy, what we're saying is "student" is just a reference to "Sandra." Anything I do to "student" will happen to "Sandra." */
+
+
+
+
+
+// Challenges
+const presidents = [
+    {name: "Donald Trump", terms: 1, party: "R"},
+    {name: "Barack Obama", terms: 2, party: "D"},
+    {name: "George W. Bush", terms: 2, party: "R"},
+    {name: "Bill Clinton", terms: 2, party: "D"}, 
+    {name: "George H. W. Bush", terms: 1, party: "R"},   
+    {name: "Ronald Reagan", terms: 2, party: "R"},
+    {name: "Jimmy Carter", terms: 1, party: "D"},
+    {name: "Gerald Ford", terms: 1, party: "R"},
+    {name: "Richard Nixon", terms: 2, party: "R"},
+    {name: "John F. Kennedy", terms: 1, party: "D"},
+    {name: "Lyndon Johnson", terms: 2, party: "D"},
+    {name: "Dwight Eisenhower", terms: 2, party: "R"}
+];
+
+// 1. filter all Democratic presidents
+const democrats = presidents.filter(function(el){
+    return el.party === "D";
+  }
+);
+
+// 2. filter all Republican, 1 term presidents
+const republicanOneTerms = presidents.filter(function(el){
+    return el.party === "R" && el.terms === 1;
+  }
+);
+
+// 3. filter last three elements of the array
+const lastThree = presidents.slice(Math.max(presidents.length - 3, 0));
+
+// 4. log all democrat, 2 term presidents
+const democratTwoTerms = presidents.filter(function(el){
+    return el.party === "D" && el.terms === 2;
+  }
+);
+console.log(democratTwoTerms);
